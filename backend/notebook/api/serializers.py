05a91b46132c1a6cfcaf77c36f717107e2637c0a
Vlad Y <blakkheart66@gmail.com>
2023-09-08 20:06:33 +0300

    chenged datetime format

--- backend/notebook/api/serializers.py
+++ backend/notebook/api/serializers.py
@@ -4,7 +4,8 @@ from notes.models import Note
 
 
 class NoteSerizalizer(ModelSerializer):
+
     class Meta:
         model = Note
-        fields = ('id', 'title', 'text','created_at', 'updated_at')
+        fields = ('id', 'title', 'text', 'created_at', 'updated_at')
         read_only_fields = ('id', 'created_at', 'updated_at')
