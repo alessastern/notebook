06bff23dc59d467fd621a0ab140178cba0e57d05
Vlad Y <blakkheart66@gmail.com>
2023-09-08 14:03:37 +0300

    added updated and creared fields to Note model, added ordering by updated field

--- backend/notebook/api/views.py
+++ backend/notebook/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from api.serializers import NoteSerizalizer
 from notes.models import Note
 
@@ -7,3 +8,5 @@ from notes.models import Note
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerizalizer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering = ('-updated_at')
--- backend/notebook/notes/models.py
+++ backend/notebook/notes/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField()
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
